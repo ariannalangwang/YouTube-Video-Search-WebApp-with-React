fdd156babc2abd30c8717416d561593269cc641d
Lang Wang <langwang@Langs-MacBook-Pro.local>
2021-02-11 13:41:00 -0800

    changed search bar

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 
 
 I made a website for end users to search YouTube videos. <br/>
-Tech stack used: html, css, javascript, React. <br/>
+Tech stack used: html, css, javascript, React and semantic-ui for styling. <br/>
 I also used YouTube's Data API to fetch data from YouTube. <br/>
 The website is hosted at: https://arianna-lang-wang-youtube-videos.vercel.app. Check it out! <br/>
-
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,6 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-
     this.props.onFormSubmit(this.state.term);
   };
 
@@ -18,12 +17,16 @@ class SearchBar extends React.Component {
       <div className="search-bar ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
+            <label><h3>Video Search</h3></label>
+            <div class="ui icon input">
+              <input
+                type="text"
+                placeholder="Search..."
+                value={this.state.term}
+                onChange={this.onInputChange}
+              />
+              <i class="search icon"></i>
+            </div>
           </div>
         </form>
       </div>
